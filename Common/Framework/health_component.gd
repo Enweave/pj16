ed2407c772a0fbdb4d463e5654ba51efff0dff0d
Enweave <enweave@gmail.com>
2025-01-21 23:30:43 +0500

    health component

---
+++ Common/Framework/health_component.gd
@@ -0,0 +1,74 @@
+﻿extends RefCounted
+
+class_name HealthComponent
+
+const FIELD_NAME: String = "health_component"
+signal OnDamage(amount: float)
+signal OnHeal(amount: float)
+signal OnDeath
+var _max_health: float    = 10.
+var current_health: float
+var is_invulnerable: bool = false
+var is_dead: bool         = false
+var element: Constants.Elements = 0
+
+func _init(max_health: float) -> void:
+    _max_health = max_health
+    current_health = _max_health
+
+
+func _element_can_damage(in_element: Constants.Elements = Constants.Elements.None) -> bool:
+    # If the element is 0, it means that the element can damage anything
+    # otherwise - rock paper scissors logic:
+    # Fire -> Earth -> Water -> Fire
+    
+    if in_element == Constants.Elements.None:
+        return true
+        
+    match in_element:
+        Constants.Elements.Fire:
+            return element == Constants.Elements.Earth
+        Constants.Elements.Water:
+            return element == Constants.Elements.Fire
+        Constants.Elements.Earth:
+            return element == Constants.Elements.Water
+        null:
+            return false 
+    
+    return true
+
+
+func update_max_health(value: float) -> void:
+    _max_health = value
+    current_health = min(current_health, _max_health)
+
+
+func damage(amount: float, in_element: Constants.Elements = Constants.Elements.None) -> bool:
+    if is_invulnerable or is_dead:
+        return false
+    
+    if not _element_can_damage(in_element):
+        return false
+
+    current_health -= amount #amount
+
+    if current_health <= 0:
+        is_dead = true
+        OnDeath.emit()
+    OnDamage.emit(amount)
+    return true
+
+func instakill() -> void:
+    is_dead = true
+    current_health = 0
+    OnDeath.emit()
+
+
+func is_full() -> bool:
+    return current_health == _max_health
+
+
+func heal(amount: float) -> void:
+    current_health = min(current_health + amount, _max_health)
+    OnHeal.emit(amount)
+	
--- Common/constants.gd
+++ Common/constants.gd
@@ -2,3 +2,10 @@ extends Node
 
 const GRID_SIZE: int = 32
 const INTERNAL_RESOLUTION: Vector2 = Vector2(640, 360)
+
+enum Elements {
+    None = 0,
+    Fire = 1,
+    Water = 2,
+    Earth = 3,
+}
