da0552cf5b5d997e291129adb168f2c3932631e5
Enweave <enweave@gmail.com>
2025-01-23 00:55:58 +0500

    remove print

--- Actors/Traps/ContactDamageTrap/contact_damage_trap.gd
+++ Actors/Traps/ContactDamageTrap/contact_damage_trap.gd
@@ -4,7 +4,6 @@ extends Node2D
 @export var damage: float = 1
 
 func _on_area_2d_body_entered(body: Node) -> void:
-    print(body)
     if HealthComponent.FIELD_NAME in body:
         var health_component: HealthComponent = body[HealthComponent.FIELD_NAME]
         health_component.damage(damage, element)
