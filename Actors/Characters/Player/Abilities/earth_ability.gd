3b5cecddf9662e69617949f6fb98a45f2d1ad7fe
Enweave <enweave@gmail.com>
2025-01-31 06:19:53 +0500

    bugfix????

--- Actors/Characters/Player/Abilities/earth_ability.gd
+++ Actors/Characters/Player/Abilities/earth_ability.gd
@@ -3,7 +3,7 @@ extends RangedWeaponBase
 func set_target(in_direction: Vector2 = Vector2.ZERO, in_position: Vector2 = Vector2.ZERO, _in_object: Node2D = null) -> void:
     _target_direction = in_direction
     _target_position = in_position
-    _target_object = null
+    
 
 func _ready() -> void:
     super._ready()
@@ -12,6 +12,7 @@ func _ready() -> void:
 
 func _on_activation() -> void:
     super._on_activation()
+    _target_object = null
 
 
 func _update_status_label(_in_text: String = "") -> void:
